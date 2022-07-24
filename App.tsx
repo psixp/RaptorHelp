ab1f7fc6ee2f9c84fb9f30c829594915892dbdb3
psixp <pablo_six@live.com>
2022-07-24 12:51:00 -0300

    update: mudando rota de visualização temporaria

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/ro
 
 import { THEME } from './src/styles/theme';
 
-import {Home} from './src/screens/Home';
+import {Register} from './src/screens/Register';
 import {Loading} from './src/components/Loading';
 
 export default function App() {
@@ -18,7 +18,7 @@ export default function App() {
         translucent={true}
       />
 
-    {fontsLoaded ? <Home /> : <Loading />}
+    {fontsLoaded ? <Register /> : <Loading />}
     </NativeBaseProvider>
   );
 }
