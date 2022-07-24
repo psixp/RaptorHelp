139b197de4c265f000d98c7236f22cbd86e1e9b8
psixp <pablo_six@live.com>
2022-07-24 12:50:02 -0300

    feature: incluindo novas telas

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,22 @@
-import { HStack, IconButton, VStack, useTheme, Text, Heading } from 'native-base';
+import { useState } from 'react';
+import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
 import { SignOut } from 'phosphor-react-native';
+import { ChatTeardropText } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_secondary.svg'
 
 import { Filter } from '../components/Filter'
+import { Button } from '../components/Button'
+
+import { Order, OrderProps } from '../components/Order'
 
 export function Home() {
     const { colors } = useTheme();
+
+    const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+    const [orders, setOrders] = useState<OrderProps[]>([
+
+    ]);
 
     return (
         /*Itens dentro do VStack ficam um ao abaixo do outro */
@@ -37,16 +47,41 @@ export function Home() {
                 </HStack>
                 <HStack space={3} mb={8}>
 
-                <Filter
-                    title="em andamento"
-                    type="open"
-                />
+                    <Filter
+                        title="em andamento"
+                        type="open"
+                        onPress={() => setStatusSelected('open')}
+                        isActive={statusSelected === 'open'}
+                    />
 
-                <Filter
-                    title="finalizados"
-                    type="closed"
+                    <Filter
+                        title="finalizados"
+                        type="closed"
+                        onPress={() => setStatusSelected('closed')}
+                        isActive={statusSelected === 'closed'}
+                    />
+                </HStack>
+                <FlatList
+                    data={orders}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => <Order data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 100 }}
+                    ListEmptyComponent={() => (
+                        <Center>
+                            <ChatTeardropText color={colors.gray[300]} size={40} />
+                            <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                                Sua lista Está vazia! {'\n'}
+                                {statusSelected === 'open' ? 'Clique no botão abaixo para adicionar um chamado' : 'Clique no botão abaixo para ver os chamados finalizados'}
+
+                            </Text>
+                        </Center>
+                    )}
                 />
-            </HStack>
+                <Button title={statusSelected === 'open'
+                    ? "Nova solicitação"
+                    : "Solicitações finalizadas"} />
+
             </VStack>
 
         </VStack>
