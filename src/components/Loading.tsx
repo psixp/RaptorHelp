21d826586413d16f4a2a11c6ac05c8e2fe6eb9b3
psixp <pablo_six@live.com>
2022-07-31 01:15:48 -0300

    feature: implementando firebase + ajustes

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,14 @@
-import { Center, Spinner} from 'native-base'
+import { Center, Spinner, Image} from 'native-base'
 
 
 export function Loading() {
     return (
         <Center flex={1} bg="gray.700">
-            <Spinner color="secundary.700" />
+            {/* <Spinner color="secundary.700" /> */}
+            <Image
+            source={require('../assets/logo-gif_4_semFundo.gif')}
+            style={{ width: 50, height: 50 }}
+             />
         </Center>
     )
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,22 +1,49 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native'
-
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
 import Logo from '../assets/logo_primary.svg';
 
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn() {
-    const [name, setName] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
 
-/*     function handleSignIn() {
-        setName("Pablito")
-    } */
 
     const { colors } = useTheme();
+
+    function handleSignIn() {
+        if (!email || !password) {
+            return Alert.alert('Entrar', 'Informe email ou senha.');
+        }
+
+        setIsLoading(true);
+
+        auth().signInWithEmailAndPassword(email, password)
+            .catch(error => {
+                console.log(error);
+                setIsLoading(false);
+
+                if (error.code === 'auth/invalid-email') {
+                    return Alert.alert('Entrar', 'Email ou senha inválida.');
+                }
+
+                if (error.code === 'auth/user-not-found') {
+                    return Alert.alert('Entrar', 'Usuário não encontrado.');
+                }
+
+                if (error.code === 'auth/wrong-password') {
+                    return Alert.alert('Entrar', 'Email ou senha inválida.');
+                }
+
+            });
+    }
+
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
             <Logo />
@@ -28,7 +55,7 @@ export function SignIn() {
             < Input placeholder="email"
                 mb={4}
                 InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-                onChangeText={setName}
+                onChangeText={setEmail}
             />
             < Input placeholder="senha"
                 InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
@@ -37,7 +64,12 @@ export function SignIn() {
                 onChangeText={setPassword}
             />
 
-            < Button title='Entrar' w="full" icon /* onPress={handleSignIn} *//>
+            < Button
+                title='Entrar'
+                w="full" icon
+                onPress={handleSignIn}
+                isLoading={isLoading}
+            />
         </VStack>
     )
 }
